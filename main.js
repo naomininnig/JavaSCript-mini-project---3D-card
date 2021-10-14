eeaff820983a46b34bf049db24429e6f398bc9b1
naomininnig <naomi.ninnig@gmail.com>
2021-10-14 11:42:01 +0200

    js

--- main.js
+++ main.js
@@ -11,15 +11,16 @@ const sizes = document.querySelector('.sizes');
 
 
 //moving animation Event
-container.addEventListener('mousemove', (e)=> {
-    let xAxis = (window.innerWidth / 2 - e.pageX )/ 15;
-    let yAxis = (window.innerHeight / 2 - e.pageY)/ 15;
+//2 divides the window in to 2 
+container.addEventListener('mousemove',(e)=> {
+    let xAxis = (window.innerWidth / 2 - e.pageX )/ 10; 
+    let yAxis = (window.innerHeight / 2 - e.pageY)/ 10;
     card.style.transform = `rotateY(${xAxis}deg)rotateX(${yAxis}deg)`
 });
 
 //Animate in
-container.addEventListener('mouseenter', (e) => {
-    card.style.transition = 'none';
+container.addEventListener('mouseenter',e => {
+    card.style.transition = 'all 0.5s ease';
 // Popout effect
 title.style.transform = "translateZ(200px)";
 sardines.style.transform = "translateZ(200px) rotateZ(45deg)";
@@ -28,9 +29,8 @@ sizes.style.transform = "translateZ(100px)";
 purches.style.transform = "translateZ(100px)";
 });
 
-
 //Animate out
-container.addEventListener('mouseleave', (e) => {
+    container.addEventListener('mouseleave', e => {
     card.style.transition = 'all 0.5s ease';
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     //Popback effect
@@ -40,3 +40,68 @@ container.addEventListener('mouseleave', (e) => {
     sizes.style.transform = "translateZ(0px)";
     purches.style.transform = "translateZ(0px)";
 });
+
+//Ingerdients Calculation 
+
+//function Conversion of Measurement Units
+function type(amount, type){
+    let conversion;
+    if(type == "weight") {
+        conversion = "g"
+    }else if(type == "liquid"){
+        conversion = "ml"
+    }else{
+        conversion = ""
+    }
+    let output = [amount, conversion]
+    return output
+}
+
+//ingerdiants object
+let FishAndChips = {
+    "Sardines Aux Aromates": type(120, "weight"),
+    "Flour": type(300,"weight"),
+    "Baking powder" : type(0.5,"weight"),
+    "Honey": type(1,"liquid"),
+    "Lager Beer": type(300,"liquid"),
+    "Cold Water": type(150,"liquid"),
+    "Potatoes (Waxy)": type(2, null),
+}
+
+// html text output
+let ulEmptyDivCard = document.querySelector(".ulEmptyDiv");
+
+//Calculation of recipe by amount of people
+function recipe(recipe,amount){
+    let arr = [];
+    for (const [name, value] of Object.entries(recipe)){
+         arr.push(`<li>${name + " " + value[0] * amount + " " + value[1]}</li>`);
+    }
+    ulEmptyDivCard.innerHTML = arr.join('');
+}
+
+//display new card
+let btn = document.querySelectorAll(".sizes button");
+let sizesDiv = document.querySelector(".sizes");
+let purchesDiv =  document.querySelector(".purches");
+let h3Div = document.querySelector(".displayNoneH3");
+
+//for each amount of people recipe amount display btn
+btn.forEach(element => {
+    element.addEventListener("click", () => {
+        sizesDiv.style.display = "none";
+        purchesDiv.style.display = "none";
+        h3Div.style.display = "none";
+        let portions = element.getAttribute("data-anchor");
+        recipe(FishAndChips, portions);
+        backBtn.style.display = "block";
+    })
+})
+//back to main card button display
+let backBtn = document.querySelector(".back");
+
+
+
+
+
+
